dd08cf5dc6d489d4df2f459851a1614c85dd5db7
Anwesh43 <anweshthecool0@gmail.com>
2019-05-04 14:51:55 +0530

    adding constants and libraries

--- bilinesquarecreatorview/src/main/java/com/anwesh/uiprojects/bilinesquarecreatorview/BiLineSquareCreatorView.kt
+++ bilinesquarecreatorview/src/main/java/com/anwesh/uiprojects/bilinesquarecreatorview/BiLineSquareCreatorView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.bilinesquarecreatorview
 /**
  * Created by anweshmishra on 04/05/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.GREEN
+val backColor : Int = Color.parseColor("#BDBDBD")
